957dd975f06169d18a4b92e174de6202cd734d27
gajun755 <gajun755@gmail.com>
2023-03-06 22:41:07 +0530

    components

---
+++ coder_dost_masterclass_2023/src/components/Video.js
@@ -0,0 +1,23 @@
+function Video(){
+
+    let topic="React JS"
+    let bg="dark"
+
+    return (
+    
+        <>
+            <img src="http://placeimg.com/160/90/any" alt="no image" />
+            <div className={bg} style={{backgroundColor:'red'}}>React JS Tutorials - Componets</div>
+        </>
+
+
+    );
+}
+
+// function Thumb(){
+//     return <div>Thumb</div>
+// }
+
+// export {Video,Thumb};
+
+export default Video;
